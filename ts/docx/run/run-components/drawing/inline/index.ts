1376edd0eaaffbfe3edcf07282b99a5f276065ca
Dolan Miu <dolan_miu@hotmail.com>
2017-04-04 01:36:57 +0100

    added inline attributes

--- ts/docx/run/run-components/drawing/inline/index.ts
+++ ts/docx/run/run-components/drawing/inline/index.ts
@@ -1,11 +1,35 @@
 import { IData } from "../../../../../media/data";
-import { XmlComponent } from "../../../../xml-components";
+import { XmlAttributeComponent, XmlComponent } from "../../../../xml-components";
 import { Graphic } from "./graphic";
+
+// distT="0" distB="0" distL="0" distR="0"
+interface IInlineAttributesProperties {
+    distT: number;
+    distB: number;
+    distL: number;
+    distR: number;
+}
+
+class InlineAttributes extends XmlAttributeComponent<IInlineAttributesProperties> {
+    protected xmlKeys = {
+        distT: "distT",
+        distB: "distB",
+        distL: "distL",
+        distR: "distR",
+    };
+}
 
 export class Inline extends XmlComponent {
 
     constructor(imageData: IData) {
         super("wp:inline");
+
+        this.root.push(new InlineAttributes({
+            distT: 0,
+            distB: 0,
+            distL: 0,
+            distR: 0,
+        }));
         this.root.push(new Graphic(imageData));
     }
 }
