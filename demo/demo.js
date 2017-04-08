f804deada109ef422832cd6bbc294617ffb17ded
Dolan Miu <dolan_miu@hotmail.com>
2017-04-09 00:26:51 +0100

    added cat to demo

--- demo/demo.js
+++ demo/demo.js
@@ -11,12 +11,16 @@ paragraph.addRun(dateText);
 doc.addParagraph(paragraph);
 
 // Feature coming soon
-// var media = new docx.Media();
-// media.addMedia("happy-penguins", "./demo/penguins.jpg");
-// var pictureRun = new docx.PictureRun(media.getMedia("happy-penguins"));
+var media = new docx.Media();
+// media.addMedia("happy-penguins", "./demo/penguins.jpeg");
+media.addMedia("happy-cats", "./demo/cats.jpeg");
+var pictureRun = new docx.PictureRun(media.getMedia("happy-cats"));
 
-// var exporter = new docx.LocalPacker(doc);
-var exporter = new docx.LocalPacker(doc);
+var paragraph = new docx.Paragraph();
+paragraph.addRun(pictureRun);
+doc.addParagraph(paragraph);
+
+var exporter = new docx.LocalPacker(doc, undefined, undefined, undefined, media);
 exporter.pack('My Document');
 
 console.log('Document created succesfully at project root!');
